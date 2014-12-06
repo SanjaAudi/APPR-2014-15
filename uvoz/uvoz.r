4821288934a900d1be1e50cdad84597c1a2aad65
Sanja Milojević <sanjaa.audi@gmail.com>
2014-12-06 15:45:00 +0100

    Dodajam projekt do sedaj, s katerim pa imam nekaj težav pri izdelavi tabel.

--- uvoz/uvoz.r
+++ uvoz/uvoz.r
@@ -1,19 +1,4 @@
 # 2. faza: Uvoz podatkov
 
-# Funkcija, ki uvozi podatke iz datoteke druzine.csv
-Audi_prodaja <- function() {
-  return(read.table("AudiByMarkets.csv", sep = ";", as.is = TRUE, skip=5,
-                   row.names = 1, na strings="-",
-                   col.names = c("1996", "1997", "1998", "1999", "2000", "2001", "2002", "2003", "2004", "2005", "2006"),
-                   fileEncoding = "Windows-1250"))
-}
-
-# Zapišimo podatke v razpredelnico druzine.
-cat("Uvažam podatke o prodaji vozil Audi od leta 1996 do leta 2006\n")
-AudiByMarkets <- Audi_prodaja()
-
-# Če bi imeli več funkcij za uvoz in nekaterih npr. še ne bi
-# potrebovali v 3. fazi, bi bilo smiselno funkcije dati v svojo
-# datoteko, tukaj pa bi klicali tiste, ki jih potrebujemo v
-# 2. fazi. Seveda bi morali ustrezno datoteko uvoziti v prihodnjih
-# fazah.
+source("podatki2faza/readcsv.r")
+#source("grafi.r")
