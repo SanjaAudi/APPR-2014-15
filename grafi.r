15de82ce3997b48e4af9b7907c0c643c024f2a36
Sanja Milojević <sanjaa.audi@gmail.com>
2014-12-07 17:23:07 +0100

    Še nedelujoč graf.

--- grafi.r
+++ grafi.r
@@ -1,4 +1,5 @@
 g1 <- apply(AudiByMarkets,1,function(x) x[1])
+g1 <- g1[!is.na(g1)]
 pdf("slike/Audi_trgi_1996.pdf")
-barplot(g1,main='Audi prodaja po trgih 1996',
-        xlab='Država', ylab='Število prodanih avtomobilov (×1000)' col=c("pink"),las=2)
+barplot(g1,main='Audi prodaja po trgih 1996', xlab='Država', ylab='Število prodanih avtomobilov (×1000)' col=c("pink"),las=2)
+dev.off()
