9affd7767f3655b6e82d190b7670bf2899859565
Sanja Milojević <sanjaa.audi@gmail.com>
2014-12-17 19:52:58 +0100

    Oddajam popravljeno s še nekaj nedelujočimi zadevami.

--- grafi.r
+++ grafi.r
@@ -3,7 +3,7 @@ g1 <- g1[!is.na(g1)]
 pdf("slike/Audi_trgi.pdf")
 barplot(apply(AudiByMarkets, 1, c)/1000, beside = TRUE,
         las=2, legend=1996:2006, args.legend=c(x="top"),
-        col=rainbow(11))
+        col=rainbow(11), cex.names=0.6)
 dev.off()
 # barplot(g1/1000,main='Audi prodaja po trgih 1996 (enako za 1997)', ylab='Število prodanih avtomobilov (×1000)', col=c("pink"),las=2,cex.names=0.6)
 # dev.off()
@@ -55,24 +55,43 @@ dev.off()
 
 f1 <- apply(AudiEmployees,1,function(x) x[1])
 pdf("slike/Audi_zaposleni_2000.pdf")
-barplot(f1/1000,main='Zaposleni v Audi tovarnah 2000', ylab='Število zaposlenih (×1000)', col=c("pink"),las=2,cex.names=0.6)
+vrstice <- rownames(AudiEmployees)
+vrstice[2] <- "Audi AG\nincl. Others"
+vrstice[7] <- "Cosworth\nTechnology
+barplot(f1/1000,main='Zaposleni v Audi tovarnah 2000',
+        ylab='Število zaposlenih (×1000)', col=c("pink"),
+        names.arg = vrstice, las=2,cex.names=0.6)
 dev.off()
 
 f2 <- apply(AudiEmployees,1,function(x) x[3])
 pdf("slike/Audi_zaposleni_2002.pdf")
-barplot(f2/1000,main='Zaposleni v Audi tovarnah 2002', ylab='Število zaposlenih (×1000)', col=c("blue"),las=2,cex.names=0.6)
+barplot(f2/1000,main='Zaposleni v Audi tovarnah 2002',
+        ylab='Število zaposlenih (×1000)',
+        col=c("blue"),las=2,cex.names=0.6)
 dev.off()
 
 f3 <- apply(AudiEmployees,1,function(x) x[4])
 pdf("slike/Audi_zaposleni_2003.pdf")
-barplot(f3/1000,main='Zaposleni v Audi tovarnah 2003', ylab='Število zaposlenih (×1000)', col=c("yellow"),las=2,cex.names=0.6)
+barplot(f3/1000,main='Zaposleni v Audi tovarnah 2003',
+        ylab='Število zaposlenih (×1000)',
+        col=c("yellow"),las=2,cex.names=0.6)
 dev.off()
 
 f4 <- apply(AudiEmployees,1,function(x) x[5])
 pdf("slike/Audi_zaposleni_2004.pdf")
-barplot(f4/1000,main='Zaposleni v Audi tovarnah 2004', ylab='Število zaposlenih (×1000)', col=c("green"),las=2,cex.names=0.6)
+barplot(f4/1000,main='Zaposleni v Audi tovarnah 2004',
+        ylab='Število zaposlenih (×1000)',
+        col=c("green"),las=2,cex.names=0.6)
 dev.off()
 
 # pdf('slike/Audi_modeli_1996.pdf')
 # pie(AudiProduction, labels = c('A3','A4','A5','A6', 'A8', 'TT', 'Lamborghini'), col=c('magenta4','lawngreen','red3','deepskyblue2'), main ='Zaposleni v Audi tovarnah')
 # dev.off()
+
+vrstice <- grep("Total|Threreof", rownames(AudiProduction),
+                ignore.case=TRUE, invert=TRUE)
+izbor <- AudiProduction[vrstice,]
+modeli <- c('A3','A4','A5','A6','A8','TT','Lamborghini')
+vsote <- sapply(modeli,
+                function(x) sum(izbor[grep(x, rownames(izbor)),11], na.rm = TRUE))
+pie(vsote)
--- podatki2faza/konzola.r
+++ podatki2faza/konzola.r
@@ -1,5 +1,5 @@
 source("podatki2faza/readcsv.r")
 
-AudiByMarkets <- branje.csv("TABELE/AudiByMarkets.csv","windows-1250",";")
-AudiEmployees <- branje.csv("TABELE/AudiEmployees.csv","windows-1250",";")
-AudiProduction <- branje.csv("TABELE/AudiProduction.csv","windows-1250",";")
+AudiByMarkets <- read.csv("TABELE/AudiByMarkets.csv","windows-1250",";")
+AudiEmployees <- read.csv("TABELE/AudiEmployees.csv","windows-1250",";")
+AudiProduction <- read.csv("TABELE/AudiProduction.csv","windows-1250",";")
