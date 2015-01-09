7d2eead8a54e4703441b890062d40ec88eccee03
Sanja Milojević <sanjaa.audi@gmail.com>
2015-01-09 23:27:55 +0100

    zemljevidi

--- grafi.r
+++ grafi.r
@@ -7,53 +7,6 @@ barplot(as.matrix(AudiByMarkets[-15,])/1000, beside = TRUE,
 legend("topleft", legend = row.names(AudiByMarkets)[1:14], cex=0.6,
        fill=rainbow(14))
 dev.off()
-# barplot(g1/1000,main='Audi prodaja po trgih 1996 (enako za 1997)', ylab='Število prodanih avtomobilov (×1000)', col=c("pink"),las=2,cex.names=0.6)
-# dev.off()
-# 
-# g2 <- apply(AudiByMarkets,1,function(x) x[3])
-# pdf("slike/Audi_trgi_1998.pdf")
-# barplot(g2/1000,main='Audi prodaja po trgih 1998', ylab='Število prodanih avtomobilov (×1000)', col=c("purple"),las=2,cex.names=0.6)
-# dev.off()
-# 
-# g3 <- apply(AudiByMarkets,1,function(x) x[4])
-# pdf("slike/Audi_trgi_1999.pdf")
-# barplot(g3/1000,main='Audi prodaja po trgih 1999', ylab='Število prodanih avtomobilov (×1000)', col=c("367"),las=2,cex.names=0.6)
-# dev.off()
-# 
-# g4 <- apply(AudiByMarkets,1,function(x) x[5])
-# pdf("slike/Audi_trgi_2000.pdf")
-# barplot(g4/1000,main='Audi prodaja po trgih 2000', ylab='Število prodanih avtomobilov (×1000)', col=c("green"),las=2,cex.names=0.6)
-# dev.off()
-# 
-# g5 <- apply(AudiByMarkets,1,function(x) x[6])
-# pdf("slike/Audi_trgi_2001.pdf")
-# barplot(g5/1000,main='Audi prodaja po trgih 2001', ylab='Število prodanih avtomobilov (×1000)', col=c("641"),las=2,cex.names=0.6)
-# dev.off()
-# 
-# g6 <- apply(AudiByMarkets,1,function(x) x[7])
-# pdf("slike/Audi_trgi_2002.pdf")
-# barplot(g6/1000,main='Audi prodaja po trgih 2002', ylab='Število prodanih avtomobilov (×1000)', col=c("508"),las=2,cex.names=0.6)
-# dev.off()
-# 
-# g7 <- apply(AudiByMarkets,1,function(x) x[8])
-# pdf("slike/Audi_trgi_2003.pdf")
-# barplot(g7/1000,main='Audi prodaja po trgih 2003', ylab='Število prodanih avtomobilov (×1000)', col=c("432"),las=2,cex.names=0.6)
-# dev.off()
-# 
-# g8 <- apply(AudiByMarkets,1,function(x) x[9])
-# pdf("slike/Audi_trgi_2004.pdf")
-# barplot(g8/1000,main='Audi prodaja po trgih 2004', ylab='Število prodanih avtomobilov (×1000)', col=c("450"),las=2,cex.names=0.6)
-# dev.off()
-# 
-# g9 <- apply(AudiByMarkets,1,function(x) x[10])
-# pdf("slike/Audi_trgi_2005.pdf")
-# barplot(g9/1000,main='Audi prodaja po trgih 2005', ylab='Število prodanih avtomobilov (×1000)', col=c("463"),las=2,cex.names=0.6)
-# dev.off()
-# 
-# g10 <- apply(AudiByMarkets,1,function(x) x[11])
-# pdf("slike/Audi_trgi_2006.pdf")
-# barplot(g10/1000,main='Audi prodaja po trgih 2006', ylab='Število prodanih avtomobilov (×1000)', col=c("118"),las=2,cex.names=0.6)
-# dev.off()
 
 f1 <- apply(AudiEmployees,1,function(x) x[1])
 pdf("slike/Audi_zaposleni_2000.pdf")
@@ -86,9 +39,6 @@ barplot(f4/1000,main='Zaposleni v Audi tovarnah 2004',
         col=c("green"),las=2,cex.names=0.6)
 dev.off()
 
-# pdf('slike/Audi_modeli_1996.pdf')
-# pie(AudiProduction, labels = c('A3','A4','A5','A6', 'A8', 'TT', 'Lamborghini'), col=c('magenta4','lawngreen','red3','deepskyblue2'), main ='Zaposleni v Audi tovarnah')
-# dev.off()
 pdf("slike/Audi_modeli.pdf")
 vrstice <- grep("Total|Threreof", rownames(AudiProduction),
                 ignore.case=TRUE, invert=TRUE)
--- vizualizacija/vizualizacija.r
+++ vizualizacija/vizualizacija.r
@@ -11,40 +11,232 @@ svet <- uvozi.zemljevid("http://www.naturalearthdata.com/http//www.naturalearthd
 
 row.names(AudiByMarkets)[3:4] <- c("United Kingdom", "United States")
 
-# Funkcija, ki podatke preuredi glede na vrstni red v zemljevidu
-# preuredi <- function(podatki, zemljevid) {
-#   nove.obcine <- c()
-#   manjkajo <- ! nove.obcine %in% rownames(podatki)
-#   M <- as.data.frame(matrix(nrow=sum(manjkajo), ncol=length(podatki)))
-#   names(M) <- names(podatki)
-#   row.names(M) <- nove.obcine[manjkajo]
-#   podatki <- rbind(podatki, M)
-#   
-#   out <- data.frame(podatki[order(rownames(podatki)), ])[rank(levels(zemljevid$OB_UIME)[rank(zemljevid$OB_UIME)]), ]
-#   if (ncol(podatki) == 1) {
-#     out <- data.frame(out)
-#     names(out) <- names(podatki)
-#     rownames(out) <- rownames(podatki)
-#   }
-#   return(out)
-# }
+# imena <- c("United States", "Europe", "China", "Japan")
+# drzave <- match(svet$name, imena.drzav)
 
-# Preuredimo podatke, da jih bomo lahko izrisali na zemljevid.
-#druzine <- preuredi(druzine, obcine)
+#LETO 1996,1997
+# Izračunamo povprečno velikost.
+min.1996 <- min(AudiByMarkets$X1996[1:13], na.rm=TRUE)
+max.1996 <- max(AudiByMarkets$X1996[1:13], na.rm=TRUE)
 
-# Izračunamo povprečno velikost družine.
+# Narišimo zemljevid v PDF.
+cat("Rišem zemljevid...\n")
+pdf("slike/zemljevid_prodaja_1996_1997.pdf")
+
+n = 100
+barve = heat.colors(n)[n:1]
+barve.svet <- barve[1+(n-1)*(AudiByMarkets$X1996-min.1996)/(max.1996-min.1996)]
+plot(svet, col = barve.svet[match(svet$name, row.names(AudiByMarkets))])
+title("Prodaja v svetu")
+dev.off()
+
+evropa <- svet[svet$continent == "Europe",]
+plot(evropa, col = barve.svet[match(evropa$name, row.names(AudiByMarkets))],
+     xlim = c(-20, 20), ylim = c(33, 73))
+title("Prodaja v Evropi")
+
+k <- 5
+koraki <- 1:k
+legend("left", legend = round(seq(min.1996, max.1996, (max.1996-min.1996)/(k-1))),
+       fill = barve[koraki*n/k])
+
+#LETO 1998
+# Izračunamo povprečno velikost.
+min.1998 <- min(AudiByMarkets$X1998[1:13], na.rm=TRUE)
+max.1998 <- max(AudiByMarkets$X1998[1:13], na.rm=TRUE)
+
+# Narišimo zemljevid v PDF.
+cat("Rišem zemljevid...\n")
+pdf("slike/zemljevid_prodaja_1998.pdf")
+
+n = 100
+barve = heat.colors(n)[n:1]
+barve.svet <- barve[1+(n-1)*(AudiByMarkets$X1998-min.1998)/(max.1998-min.1998)]
+plot(svet, col = barve.svet[match(svet$name, row.names(AudiByMarkets))])
+title("Prodaja v svetu")
+dev.off()
+
+evropa <- svet[svet$continent == "Europe",]
+plot(evropa, col = barve.svet[match(evropa$name, row.names(AudiByMarkets))],
+     xlim = c(-20, 20), ylim = c(33, 73))
+title("Prodaja v Evropi")
+
+k <- 5
+koraki <- 1:k
+legend("left", legend = round(seq(min.1998, max.1998, (max.1998-min.1998)/(k-1))),
+       fill = barve[koraki*n/k])
+
+#LETO 1999,2000
+# Izračunamo povprečno velikost.
+min.1999 <- min(AudiByMarkets$X1999[1:13], na.rm=TRUE)
+max.1999 <- max(AudiByMarkets$X1999[1:13], na.rm=TRUE)
+
+# Narišimo zemljevid v PDF.
+cat("Rišem zemljevid...\n")
+pdf("slike/zemljevid_prodaja_1999_2000.pdf")
+
+n = 100
+barve = heat.colors(n)[n:1]
+barve.svet <- barve[1+(n-1)*(AudiByMarkets$X1999-min.1999)/(max.1999-min.1999)]
+plot(svet, col = barve.svet[match(svet$name, row.names(AudiByMarkets))])
+title("Prodaja v svetu")
+dev.off()
+
+evropa <- svet[svet$continent == "Europe",]
+plot(evropa, col = barve.svet[match(evropa$name, row.names(AudiByMarkets))],
+     xlim = c(-20, 20), ylim = c(33, 73))
+title("Prodaja v Evropi")
+
+k <- 5
+koraki <- 1:k
+legend("left", legend = round(seq(min.1999, max.1999, (max.1999-min.1999)/(k-1))),
+       fill = barve[koraki*n/k])
+
+
+#LETO 2001
+# Izračunamo povprečno velikost.
+min.2001 <- min(AudiByMarkets$X2001[1:13], na.rm=TRUE)
+max.2001 <- max(AudiByMarkets$X2001[1:13], na.rm=TRUE)
+
+# Narišimo zemljevid v PDF.
+cat("Rišem zemljevid...\n")
+pdf("slike/zemljevid_prodaja_2001.pdf")
+
+n = 100
+barve = heat.colors(n)[n:1]
+barve.svet <- barve[1+(n-1)*(AudiByMarkets$X2001-min.2001)/(max.2001-min.2001)]
+plot(svet, col = barve.svet[match(svet$name, row.names(AudiByMarkets))])
+title("Prodaja v svetu")
+dev.off()
+
+evropa <- svet[svet$continent == "Europe",]
+plot(evropa, col = barve.svet[match(evropa$name, row.names(AudiByMarkets))],
+     xlim = c(-20, 20), ylim = c(33, 73))
+title("Prodaja v Evropi")
+
+k <- 5
+koraki <- 1:k
+legend("left", legend = round(seq(min.2001, max.2001, (max.2001-min.2001)/(k-1))),
+       fill = barve[koraki*n/k])
+
+#LETO 2002
+# Izračunamo povprečno velikost.
+min.2002 <- min(AudiByMarkets$X2002[1:13], na.rm=TRUE)
+max.2002 <- max(AudiByMarkets$X2002[1:13], na.rm=TRUE)
+
+# Narišimo zemljevid v PDF.
+cat("Rišem zemljevid...\n")
+pdf("slike/zemljevid_prodaja_2002.pdf")
+
+n = 100
+barve = heat.colors(n)[n:1]
+barve.svet <- barve[1+(n-1)*(AudiByMarkets$X2002-min.2002)/(max.2002-min.2002)]
+plot(svet, col = barve.svet[match(svet$name, row.names(AudiByMarkets))])
+title("Prodaja v svetu")
+dev.off()
+
+evropa <- svet[svet$continent == "Europe",]
+plot(evropa, col = barve.svet[match(evropa$name, row.names(AudiByMarkets))],
+     xlim = c(-20, 20), ylim = c(33, 73))
+title("Prodaja v Evropi")
+
+k <- 5
+koraki <- 1:k
+legend("left", legend = round(seq(min.2002, max.2002, (max.2002-min.2002)/(k-1))),
+       fill = barve[koraki*n/k])
+
+#LETO 2003
+# Izračunamo povprečno velikost.
+min.2003 <- min(AudiByMarkets$X2003[1:13], na.rm=TRUE)
+max.2003 <- max(AudiByMarkets$X2003[1:13], na.rm=TRUE)
+
+# Narišimo zemljevid v PDF.
+cat("Rišem zemljevid...\n")
+pdf("slike/zemljevid_prodaja_2003.pdf")
+
+n = 100
+barve = heat.colors(n)[n:1]
+barve.svet <- barve[1+(n-1)*(AudiByMarkets$X2003-min.2003)/(max.2003-min.2003)]
+plot(svet, col = barve.svet[match(svet$name, row.names(AudiByMarkets))])
+title("Prodaja v svetu")
+dev.off()
+
+evropa <- svet[svet$continent == "Europe",]
+plot(evropa, col = barve.svet[match(evropa$name, row.names(AudiByMarkets))],
+     xlim = c(-20, 20), ylim = c(33, 73))
+title("Prodaja v Evropi")
+
+k <- 5
+koraki <- 1:k
+legend("left", legend = round(seq(min.2003, max.2003, (max.2003-min.2003)/(k-1))),
+       fill = barve[koraki*n/k])
+
+#LETO 2004
+# Izračunamo povprečno velikost.
+min.2004 <- min(AudiByMarkets$X2004[1:13], na.rm=TRUE)
+max.2004 <- max(AudiByMarkets$X2004[1:13], na.rm=TRUE)
+
+# Narišimo zemljevid v PDF.
+cat("Rišem zemljevid...\n")
+pdf("slike/zemljevid_prodaja_2004.pdf")
+
+n = 100
+barve = heat.colors(n)[n:1]
+barve.svet <- barve[1+(n-1)*(AudiByMarkets$X2004-min.2004)/(max.2004-min.2004)]
+plot(svet, col = barve.svet[match(svet$name, row.names(AudiByMarkets))])
+title("Prodaja v svetu")
+dev.off()
+
+evropa <- svet[svet$continent == "Europe",]
+plot(evropa, col = barve.svet[match(evropa$name, row.names(AudiByMarkets))],
+     xlim = c(-20, 20), ylim = c(33, 73))
+title("Prodaja v Evropi")
+
+k <- 5
+koraki <- 1:k
+legend("left", legend = round(seq(min.2004, max.2004, (max.2004-min.2004)/(k-1))),
+       fill = barve[koraki*n/k])
+
+#LETO 2005
+# Izračunamo povprečno velikost.
+min.2005 <- min(AudiByMarkets$X2005[1:13], na.rm=TRUE)
+max.2005 <- max(AudiByMarkets$X2005[1:13], na.rm=TRUE)
+
+# Narišimo zemljevid v PDF.
+cat("Rišem zemljevid...\n")
+pdf("slike/zemljevid_prodaja_2005.pdf")
+
+n = 100
+barve = heat.colors(n)[n:1]
+barve.svet <- barve[1+(n-1)*(AudiByMarkets$X2005-min.2005)/(max.2005-min.2005)]
+plot(svet, col = barve.svet[match(svet$name, row.names(AudiByMarkets))])
+title("Prodaja v svetu")
+dev.off()
+
+evropa <- svet[svet$continent == "Europe",]
+plot(evropa, col = barve.svet[match(evropa$name, row.names(AudiByMarkets))],
+     xlim = c(-20, 20), ylim = c(33, 73))
+title("Prodaja v Evropi")
+
+k <- 5
+koraki <- 1:k
+legend("left", legend = round(seq(min.2005, max.2005, (max.2005-min.2005)/(k-1))),
+       fill = barve[koraki*n/k])
+
+#LETO 2006
+# Izračunamo povprečno velikost.
 min.2006 <- min(AudiByMarkets$X2006[1:13], na.rm=TRUE)
 max.2006 <- max(AudiByMarkets$X2006[1:13], na.rm=TRUE)
 
 # Narišimo zemljevid v PDF.
 cat("Rišem zemljevid...\n")
-pdf("slike/zemljevid_prodaja.pdf")
+pdf("slike/zemljevid_prodaja_2006.pdf")
 
 n = 100
 barve = heat.colors(n)[n:1]
 barve.svet <- barve[1+(n-1)*(AudiByMarkets$X2006-min.2006)/(max.2006-min.2006)]
 plot(svet, col = barve.svet[match(svet$name, row.names(AudiByMarkets))])
-
+title("Prodaja v svetu")
 dev.off()
 
 evropa <- svet[svet$continent == "Europe",]
