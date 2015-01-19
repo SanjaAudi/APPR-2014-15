5e1a21b57e399d581c0eb8f861ac191008b8a18a
Sanja Milojević <sanjaa.audi@gmail.com>
2015-01-19 20:36:23 +0100

    Imena na zemljevidih.

--- vizualizacija/vizualizacija.r
+++ vizualizacija/vizualizacija.r
@@ -12,13 +12,23 @@ svet <- uvozi.zemljevid("http://www.naturalearthdata.com/http//www.naturalearthd
 row.names(AudiByMarkets)[3:4] <- c("United Kingdom", "United States")
 
 evropa <- svet[svet$continent == "Europe",]
-m <- match(row.names(AudiByMarkets), evropa$name)
-koordinate <- coordinates(evropa)
-rownames(koordinate) <- evropa$name
-imena <- as.character(evropa$name)
-names(imena) <- evropa$name
+rest <- c("United States", "China", "Japan")
+m <- match(row.names(AudiByMarkets), svet$name)
+n <- match(rest, svet$name)
+koordinate <- coordinates(svet)
+rownames(koordinate) <- svet$name
+imena <- as.character(svet$name)
+names(imena) <- svet$name
 
 imena["United Kingdom"] <- "United\nKingdom"
+imena["United States"] <- "United\nStates"
+imena["Netherlands"] <- "Netherl."
+imena["Switzerland"] <- "Switzer."
+koordinate["Sweden",1] <- koordinate["Sweden",1] - 1
+koordinate["United Kingdom",1] <- koordinate["United Kingdom",1] + 1
+koordinate["United Kingdom",2] <- koordinate["United Kingdom",2] - 1
+koordinate["Austria",1] <- koordinate["Austria",1] + 0.5
+
 
 #LETO 2001
 # Izračunamo povprečno velikost.
@@ -33,6 +43,7 @@ n = 100
 barve = cm.colors(n)[n:1]
 barve.svet <- barve[1+(n-1)*(AudiByMarkets$X2001-min.2001)/(max.2001-min.2001)]
 plot(svet, col = barve.svet[match(svet$name, row.names(AudiByMarkets))])
+#text(koordinate[n,], labels = rest, cex = 0.5)
 title("Prodaja v svetu leta 2001")
 
 k <- 5
@@ -45,6 +56,7 @@ pdf("slike/zemljevid_evropa_2001.pdf")
 evropa <- svet[svet$continent == "Europe",]
 plot(evropa, col = barve.svet[match(evropa$name, row.names(AudiByMarkets))],
      xlim = c(-20, 20), ylim = c(33, 73))
+text(koordinate[m,], labels = imena[m], cex = 0.5)
 title("Prodaja v Evropi leta 2001")
 
 k <- 5
@@ -77,6 +89,7 @@ pdf("slike/zemljevid_evropa_2002.pdf")
 evropa <- svet[svet$continent == "Europe",]
 plot(evropa, col = barve.svet[match(evropa$name, row.names(AudiByMarkets))],
      xlim = c(-20, 20), ylim = c(33, 73))
+text(koordinate[m,], labels = imena[m], cex = 0.5)
 title("Prodaja v Evropi leta 2002")
 
 k <- 5
@@ -109,6 +122,7 @@ pdf("slike/zemljevid_evropa_2003.pdf")
 evropa <- svet[svet$continent == "Europe",]
 plot(evropa, col = barve.svet[match(evropa$name, row.names(AudiByMarkets))],
      xlim = c(-20, 20), ylim = c(33, 73))
+text(koordinate[m,], labels = imena[m], cex = 0.5)
 title("Prodaja v Evropi leta 2003")
 
 k <- 5
@@ -141,6 +155,7 @@ pdf("slike/zemljevid_evropa_2004.pdf")
 evropa <- svet[svet$continent == "Europe",]
 plot(evropa, col = barve.svet[match(evropa$name, row.names(AudiByMarkets))],
      xlim = c(-20, 20), ylim = c(33, 73))
+text(koordinate[m,], labels = imena[m], cex = 0.5)
 title("Prodaja v Evropi leta 2004")
 
 k <- 5
@@ -173,6 +188,7 @@ pdf("slike/zemljevid_evropa_2005.pdf")
 evropa <- svet[svet$continent == "Europe",]
 plot(evropa, col = barve.svet[match(evropa$name, row.names(AudiByMarkets))],
      xlim = c(-20, 20), ylim = c(33, 73))
+text(koordinate[m,], labels = imena[m], cex = 0.5)
 title("Prodaja v Evropi leta 2005")
 
 k <- 5
@@ -205,6 +221,7 @@ pdf("slike/zemljevid_evropa_2006.pdf")
 evropa <- svet[svet$continent == "Europe",]
 plot(evropa, col = barve.svet[match(evropa$name, row.names(AudiByMarkets))],
      xlim = c(-20, 20), ylim = c(33, 73))
+text(koordinate[m,], labels = imena[m], cex = 0.5)
 title("Prodaja v Evropi leta 2006")
 
 k <- 5
